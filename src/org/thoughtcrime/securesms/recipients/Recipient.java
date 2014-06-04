88725988fa3f22fdbe88b663eb4dca36aac195fe
Bastian Köcher <git@kchr.de>
2014-06-04 19:40:07 +0200

    Revert "Added first changes to support updating recipients without recreating them." This feature needs more time and another way of solving it.
    This reverts commit db42bfe0903dc8c7a187ab84795c5f3954e47c9f.

--- src/org/thoughtcrime/securesms/recipients/Recipient.java
+++ src/org/thoughtcrime/securesms/recipients/Recipient.java
@@ -114,6 +114,25 @@ public class Recipient implements Parcelable, CanonicalRecipient {
         }
       }
 
+    future.setListener(new FutureTaskListener<RecipientDetails>() {
+      @Override
+      public void onSuccess(RecipientDetails result) {
+        if (result != null) {
+          HashSet<RecipientModifiedListener> localListeners;
+
+          synchronized (Recipient.this) {
+            Recipient.this.name         = result.name;
+            Recipient.this.contactUri   = result.contactUri;
+            Recipient.this.contactPhoto = result.avatar;
+            localListeners              = (HashSet<RecipientModifiedListener>)listeners.clone();
+            listeners.clear();
+          }
+
+          for (RecipientModifiedListener listener : localListeners)
+            listener.onModified(Recipient.this);
+        }
+      }
+
       @Override
       public void onFailure(Throwable error) {
         Log.w("Recipient", error);
